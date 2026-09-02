5e3cfad9e7abbc43de4b61e2131334f117daeaaa
Imane Mechali <e2182561@cmaisonneuve.qc.ca>
2026-09-02 11:30:36 -0400

    im amazing

---
+++ teeshirts.php
@@ -0,0 +1,17 @@
+<?php
+//inclure le fichier commun contenant le haut du code des écrans de UI
+
+include("commun/entete.inc.php");
+
+ ?> 
+        <main class="page-teeshirts">
+            <article class="amorce">
+                <h1>Nos teeshirts</h1>
+            </article>
+            <article class="principal">
+                À venir...
+            </article>
+        </main>
+        <?php 
+include("commun/p2p.inc.php");
+?> 
